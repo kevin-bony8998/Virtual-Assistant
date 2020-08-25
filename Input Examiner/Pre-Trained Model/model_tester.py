4269487c734b51d26efc56beb311912c15866ae8
Kevin Bony <59431001+kevin-bony8998@users.noreply.github.com>
2020-08-26 03:42:29 +0900

    Add files via upload

---
+++ Input Examiner/Pre-Trained Model/model_tester.py
@@ -0,0 +1,7 @@
+from gensim.models import Word2Vec
+
+model = Word2Vec.load("C:/Users/KEVINBONYTHEKKANATH-/Desktop/Projects/Chatbot/word_similarity")
+print(model["war"])
+print(model.wv.index2entity[:100])
+print(model.wv.similarity('london', 'adele'))
+print(len(model.wv.vocab))
