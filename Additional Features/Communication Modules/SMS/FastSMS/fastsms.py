dfa30afdce2f80c8f04ff6868c5ff8713144efb0
Kevin Bony <59431001+kevin-bony8998@users.noreply.github.com>
2020-08-26 03:28:12 +0900

    Add files via upload

---
+++ Additional Features/Communication Modules/SMS/FastSMS/fastsms.py
@@ -0,0 +1,14 @@
+import requests
+
+url = "https://www.fast2sms.com/dev/bulk"
+
+num="9880005778"
+querystring = {"authorization":"iZWfNxwR5OIbrtpCkHUq3eB0sY81QnvcgPh6VaDJj2SoAEldu7voO93nRemWXqYxCyMBK5LraGU7ht6j","sender_id":"FSTSMS","message":"This is test message","language":"english","route":"p","numbers":num}
+
+headers = {
+    'cache-control': "no-cache"
+}
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
