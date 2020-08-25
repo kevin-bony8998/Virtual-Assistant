72879cbefb7f37c58b17eca355403984228b4718
Kevin Bony <59431001+kevin-bony8998@users.noreply.github.com>
2020-08-26 00:56:26 +0530

    Update sms_sender_twilio.py

--- Additional Features/Communication Modules/SMS/Twilio/sms_sender_twilio.py
+++ Additional Features/Communication Modules/SMS/Twilio/sms_sender_twilio.py
@@ -3,15 +3,15 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'ACab2e11ac1e31bfcf526812013a5521f8'
-auth_token = '3d3978488324a8a8c8000b37ae78acdd'
+account_sid = '<id>'
+auth_token = '<your_token>'
 client = Client(account_sid, auth_token)
 
 message = client.messages \
     .create(
          body='This is the ship that made the Kessel Run in fourteen parsecs?',
-         from_='+14159806287',
-         to='+918618824813'
+         from_='<number_received from twilio>',
+         to='<to_number>'
      )
 
 print(message.sid)
